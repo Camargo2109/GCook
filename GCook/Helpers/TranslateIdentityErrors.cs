a4fc719dd4058019faedd42077eab4fa7b9c0bbc
Arthur Camargo <arthur.barrabonita@gmail.com>
2024-09-17 10:42:54 -0300

    terminar translate

--- GCook/Helpers/TranslateIdentityErrors.cs
+++ GCook/Helpers/TranslateIdentityErrors.cs
@@ -1,12 +1,22 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+namespace GCook.Helpers;
 
-namespace GCook.Helpers
+public static class TranslateIdentityErros
 {
-    public class TranslateIdentityErrors
+    public static string TranslateErrorsMessage(string codeError)
     {
-        
+        string message = codeError switch
+        {
+            "DefaultError" => "Ocorreu um erro desconhecido",
+            "ConcurrencyFailure" => "Falha de concorrência otimista, o objeto foi modificado.",
+            "InvalidToken" => "Token invalido.",
+            "LoginAlreadyAssociated" => "Já existe um usuário com este login.",
+            "InvalidUserName" => $"Este login é inválido, um login deve cnter apenas letras ou dígitos.",
+            "InvalidEmail" => "E-mail inválido.",
+            "DuplicateUserName" => "Este login já está sendo utilizado",
+            "DuplicateEmail" => $"Este email já está sendo utilizado",
+            "InvalidRoleName" => "Esta permissão ja esta sendo Utilizada"
+            
+        };
     }
 }
+
