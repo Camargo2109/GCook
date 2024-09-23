9af570fd6265f58bb6ba38f37fb042958686e774
Arthur Camargo <arthur.barrabonita@gmail.com>
2024-09-23 08:13:55 -0300

    terminar usuarioservice

--- GCook/Services/UsuarioService.cs
+++ GCook/Services/UsuarioService.cs
@@ -1,12 +1,49 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using GCook.Data;
+using GCook.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using GCook.Helpers;
+using GCook.Models;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Text;
+using System.Text.Encodings.Web;
 
-namespace GCook.Services
+
+namespace GCook.Services;
+
+public class UsuarioSevice : IUsuarioService
 {
-    public class UsuarioService
+    private readonly AppDbContext _contexto;
+    private readonly SignInManager<IdentityUser> _signManager;
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly IHttpContextAccessor httpContextAccessor;
+    private readonly IUserStore<IdentityUser> userStore;
+    private readonly IUserEmailStore<IdentityUser> _emailStore;
+    private readonly IWebHostEnvironment _hostEnvironment;
+    private readonly IEmailSender _emailSender;
+    private readonly ILogger<UsuarioService> _logger;
+
+
+    public UsuarioSevice(
+        AppDbContext contexto,
+        SignInManager<IdentityUser> signInManager,
+        UserManager<IdentityUser> userManager,
+        IHttpContextAccessor httpContextAccessor,
+        IUserStore<IdentityUser> userStore,
+        IWebHostEnvironment hostEnvironment,
+        IEmailSender emailSender,
+        ILogger<UsuarioSevice> logger
+    )
     {
-        
+        _contexto = contexto;
+        _signManager = signInManager;
+        _userManager = userManager;
+        _httpContextAccessor = httpContextAccessor;
+        _userStore = userStore;
+        _emailStore = (IUserEmailStore<IdentityUser>)_userStore;
+        _hostEnvironment = hostEnvironment;
+        _emailSender = emailSender;
+        _logger = logger;
     }
 }
